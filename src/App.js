70244e45ad9c0310605b71c2d5f82b9809baa221
komalsalunkhe01 <89084215+komalsalunkhe01@users.noreply.github.com>
2021-08-17 19:00:15 +0530

    hotel website files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+//import UseState from "./compenent/hooks/useState";
+//import Restaurant from './compenent/Basics/Restaurant'
+// import UseEffect from "./compenent/hooks/UseEffect" 
+import UseReducer from "./compenent/hooks/UseReducer" 
+const App = () => {
+  return (
+    <>
+      <UseReducer/>
+    </>
+  )
+}
+
+export default App
